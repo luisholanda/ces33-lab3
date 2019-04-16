ec91c03ec146c71f16827d72aba8a1b5e8a6b322
Felipe <felipecoimbra97@gmail.com>
2019-04-16 18:11:47 -0300

    Adding matrix class

---
+++ Matrix.hpp
@@ -0,0 +1,136 @@
+#ifndef LAB3_MATRIX_HPP
+#define LAB3_MATRIX_HPP
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cassert>
+
+template<typename T = double>
+class Matrix {
+public:
+    using RandomAllowedMatrix = Matrix<typename std::enable_if<std::is_floating_point<T>::value, T>::type>;
+
+    /*!
+    *   \brief Default initializer
+    */
+    Matrix<T>(size_t nrows, size_t ncols) 
+        : nrows(nrows)
+        , ncols(ncols)
+        , data(nrows, RowCont(ncols))
+    {}
+
+    Matrix<T>(const Matrix<T>& copy) = default;
+
+    /*!
+    *   \brief Creates a matrix with uniformly distributed random numbers in [0, 1] if T is a real type
+    */
+    static RandomAllowedMatrix
+    random(size_t nrows, size_t ncols) {
+        Matrix<T> matrix(nrows, ncols);
+        std::random_device dev;
+        std::default_random_engine generator(dev());
+        std::uniform_real_distribution<T> distribution(0.0, 1.0);
+        for (auto& row : matrix.data) {
+            for (auto& elem : row) {
+                elem =  distribution(generator);
+            }
+        }
+        return matrix;
+    }
+
+    /*!
+    *   \brief Prints matrix do stdout
+    */
+    void print() {
+        for (const auto& row : data) {
+            for (const auto& elem : row) {
+                std::cout << elem << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    Matrix<T>& transpose() {
+        MatrixCont old = data;
+        std::swap(nrows, ncols);
+        data.clear();
+        data.assign(nrows, RowCont(ncols));
+        for (size_t i = 0; i < nrows; i++) {
+            for (size_t j = 0; j < ncols; j++) {
+                data[i][j] = old[j][i];
+            }
+        }
+        return *this;
+    }
+
+
+    Matrix<T> operator + (const Matrix<T>& other) {
+        assert (nrows == other.nrows && ncols == other.ncols);
+        Matrix<T> result(*this);
+        MatrixCont& resultData = result.data;
+        const MatrixCont& otherData = other.data;
+        for (size_t i = 0; i < other.nrows; i++) {
+            for (size_t j = 0; j < other.ncols; j++) {
+                resultData[i][j] = resultData[i][j] + otherData[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix<T>& operator += (const Matrix<T>& other) {
+        assert (nrows == other.nrows && ncols == other.ncols);
+        MatrixCont& resultData = this->data;
+        const MatrixCont& otherData = other.data;
+        for (size_t i = 0; i < other.nrows; i++) {
+            for (size_t j = 0; j < other.ncols; j++) {
+                resultData[i][j] = resultData[i][j] + otherData[i][j];
+            }
+        }
+        return *this;
+    }
+
+    Matrix<T> operator * (const Matrix<T>& other) {
+        assert (ncols == other.nrows);
+        Matrix<T> result(nrows, other.ncols);
+        MatrixCont& resultData = result.data;
+        const MatrixCont& otherData = other.data;
+        for (size_t i = 0; i < nrows; i++) {
+            for (size_t j = 0; j < other.ncols; j++) {
+                T& resultElem = resultData[i][j];
+                for (size_t k = 0; k < ncols; k++) {
+                    resultElem = resultElem + data[i][k] * otherData[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+     Matrix<T>& operator *= (const Matrix<T>& other) {
+        assert (ncols == other.nrows);
+        const MatrixCont& otherData = other.data;
+        RowCont oldData;
+        for (size_t i = 0; i < nrows; i++) {
+            oldData = data[i];
+            data[i].resize(other.ncols);
+            for (size_t j = 0; j < other.ncols; j++) {
+                auto resultElem = T();
+                for (size_t k = 0; k < ncols; k++) {
+                    resultElem = resultElem + oldData[k] * otherData[k][j];
+                }
+                data[i][j] = resultElem;
+            }
+        }
+        return *this;
+    }
+
+private:
+    using RowCont = std::vector<T>;     //!< Container for rows
+    using MatrixCont = std::vector<RowCont>; //!< Container for columns
+
+    size_t nrows;   //!< Number of rows
+    size_t ncols;   //!< Number of columns
+    MatrixCont data;    //!< Data container
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "Matrix.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto m = Matrix<>::random(2, 3), n = Matrix<>::random(2, 3);
+    m.print();
+    std::cout << std::endl;
+    n.print();
+    std::cout << std::endl;
+    (m + n).print();
+    std::cout << std::endl;
+    (m += n).print();
+    std::cout << std::endl;
+    (m.transpose()).print();
+    std::cout << std::endl;
+    (m * n).print();
+    std::cout << std::endl;
+    (m *= n).print();
     return 0;
 }
